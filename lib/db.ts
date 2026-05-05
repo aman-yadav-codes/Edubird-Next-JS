60e19b563a847600e60460096e0a3d310c21ebfd
aman-yadav-codes <amanyadavcodes@gmail.com>
2026-05-05 16:33:33 +0530

    added the neon database

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,17 @@
-// /lib/db.ts
+// // /lib/db.ts
+// import { Pool } from "pg";
+
+// export const db = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "edubird_db",
+//   password: "admin123",
+//   port: 5432,
+// });
+
 import { Pool } from "pg";
 
 export const db = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "edubird_db",
-  password: "admin123",
-  port: 5432,
+  connectionString:
+    "postgresql://neondb_owner:npg_L7NEbuc5FlpA@ep-noisy-wind-an8c6m9x-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
