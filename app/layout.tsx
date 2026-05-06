b627356b7532c27492d36326d3b58e266b41211e
aman-yadav-codes <amanyadavcodes@gmail.com>
2026-05-06 09:38:18 +0530

    testing again the api via pushing on vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,19 +26,8 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      suppressHydrationWarning
     >
-      <body suppressHydrationWarning className="min-h-full flex flex-col">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <body suppressHydrationWarning className="min-h-full flex flex-col">{children}</body>
     </html>
-  );  
+  );
 }
- 
--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,9 @@
-// // /lib/db.ts
-// import { Pool } from "pg";
-
-// export const db = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "edubird_db",
-//   password: "admin123",
-//   port: 5432,
-// });
-
+// /lib/db.ts
 import { Pool } from "pg";
 
 export const db = new Pool({
-  connectionString:
-    "postgresql://neondb_owner:npg_L7NEbuc5FlpA@ep-noisy-wind-an8c6m9x-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    connectionString:"postgresql://neondb_owner:npg_L7NEbuc5FlpA@ep-noisy-wind-an8c6m9x-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
